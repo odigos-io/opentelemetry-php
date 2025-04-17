e5a6d7cbdc7f9127a0e3c02817fa17004da7b5e0
Ben Elferink <ben.elferink@icloud.com>
2025-04-17 23:53:10 +0300

    fix: add version check for opentelemetry PHP extension

--- 8.1/index.php
+++ 8.1/index.php
@@ -4,3 +4,5 @@ require dirname(__DIR__) . '/8.1/vendor/autoload.php';
 // The versioned path is for Odigos auto-instrumentation (Odiglet)
 // If you need to use this in your own code, you can use the following line instead:
 // require 'vendor/autoload.php';
+
+printf('The PHP extension "opentelemetry" is available, version %s', phpversion('opentelemetry'));
