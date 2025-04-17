cb6b0681013643201a6de0ff84ebe3362f81f751
Ben Elferink <ben.elferink@icloud.com>
2025-04-17 23:44:31 +0300

    chore: add comment

--- 8.1/index.php
+++ 8.1/index.php
@@ -1,7 +1,6 @@
 <?php
 
-namespace Odigos\OpenTelemetry;
-
 require dirname(__DIR__) . '/8.1/vendor/autoload.php';
-
-printf('The PHP extension "opentelemetry" is available, version %s', phpversion('opentelemetry'));
+// The versioned path is for Odigos auto-instrumentation (Odiglet)
+// If you need to use this in your own code, you can use the following line instead:
+// require 'vendor/autoload.php';
